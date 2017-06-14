9c8f6a61269ee872c7d23a75cac3812bb151202a
s-ichikawa <ichikawa.shingo.0829@gmail.com>
2017-06-14 23:46:46 +0900

    amp

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,9 +6,11 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    //
     public function index()
     {
-        return view('index');
+        $contents = [];
+        return view('index', [
+            'contents' => $contents,
+        ]);
     }
 }
