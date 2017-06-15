d88b024771fadc07d3525a61cebc104e248d203b
s-ichikawa <ichikawa.shingo.0829@gmail.com>
2017-06-15 23:05:18 +0900

    top page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.base')
 
+@section('title', 'TOP')
+
 @section('contents')
     Hello Laravel!
 @endsection
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,26 +1,20 @@
 <!doctype html>
-<html amp lang="en">
+<html lang="ja">
 <head>
     <meta charset="utf-8">
-    <script async src="https://cdn.ampproject.org/v0.js"></script>
-    <title>Hello, AMPs</title>
-    <link rel="canonical" href="http://example.ampproject.org/article-metadata.html" />
+    <title>Beer&Tech @yield('title')</title>
     <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
-    <script type="application/ld+json">
-      {
-        "@context": "http://schema.org",
-        "@type": "NewsArticle",
-        "headline": "Open-source framework for publishing content",
-        "datePublished": "2015-10-07T12:02:41Z",
-        "image": [
-          "logo.jpg"
-        ]
-      }
-    </script>
-    <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
 </head>
 <body>
+<header>
+    <h1>@yield('headline')</h1>
+</header>
+
 @section('contents')
 @show
+
+<footer>
+
+</footer>
 </body>
 </html>
